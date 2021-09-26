01b8019033545d95a168cb34b3542da3afd99b1b
僚 <>
2021-09-26 15:56:47 +0900

    既存カテゴリー名での登録時のテスト実装

--- spec/requests/api/v1/categories_spec.rb
+++ spec/requests/api/v1/categories_spec.rb
@@ -3,16 +3,27 @@ require "rails_helper"
 RSpec.describe "Api::V1::Categories", type: :request do
   describe "POST /api/v1/categories" do
     subject { post(api_v1_categories_path, params: params) }
+
     context "nameとbodyに必要な情報が存在するとき" do
       let(:params) { attributes_for(:category).merge(attributes_for(:idea)) }
       it "カテゴリーとアイデアの作成に成功する" do
-        expect{ subject }.to change {Idea.count}.by(1)
+        expect { subject }.to change { Idea.count }.by(1)
         res = JSON.parse(response.body)
         expect(res["status"]).to eq "201"
         expect(response).to have_http_status(:ok)
       end
     end
 
-
+    context "カテゴリーが既に存在するとき" do
+      let!(:category) { create(:category, name: "既存カテゴリー") }
+      let(:params) { attributes_for(:category, name: "既存カテゴリー").merge(attributes_for(:idea)) }
+      it "アイデアだけ作成される" do
+        expect { subject }.to change { Idea.count }.by(1) &
+                              not_change { Category.count }
+        res = JSON.parse(response.body)
+        expect(res["status"]).to eq "201"
+        expect(response).to have_http_status(:ok)
+      end
+    end
   end
 end
