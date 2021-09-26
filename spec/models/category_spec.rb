586d3f1ea28593ef0bf1875f32d328101a16a4ec
僚 <>
2021-09-26 11:52:53 +0900

    カテゴリーモデルのモデルテストの実装

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,31 @@
 require "rails_helper"
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "正常系" do
+    context "nameが一意のとき" do
+      let(:category) { build(:category) }
+      it "カテゴリーの作成に成功する" do
+        expect(category).to be_valid
+      end
+    end
+  end
+
+  describe "異常系" do
+    context "nameが空のとき" do
+      let(:category) { build(:category, name: nil) }
+      it "カテゴリーの作成に失敗する" do
+        expect(category).to be_invalid
+        expect(category.errors.details[:name][0][:error]).to eq :blank
+      end
+    end
+
+    context "nameが重複するとき" do
+      let!(:first_category) { create(:category, name: "青") }
+      let(:category) { build(:category, name: "青") }
+      it "カテゴリーの作成に失敗する" do
+        expect(category).to be_invalid
+        expect(category.errors.details[:name][0][:error]).to eq :taken
+      end
+    end
+  end
 end
