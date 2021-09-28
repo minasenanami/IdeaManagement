e56ed37de07a7dc13fcbd6a534bb47170a631d39
僚 <>
2021-09-28 15:35:38 +0900

    アイデア一覧の取得機能の実装

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -5,7 +5,42 @@ class Api::V1::CategoriesController < ApplicationController
     render json: { status: "201" }
   end
 
+  def index
+    category_word = Category.where(name: params[:name])
+    result = Idea.find_by(category_id: category_word.ids)
+    no_match = Idea.order(created_at: :asc).includes(:category)
+
+    if result
+      render json: match_result(result)
+    else
+      render json: not_match_result(no_match)
+    end
+  end
+
   private
+
+    def not_match_result(not_match_items)
+      not_match_items.map do |item|
+        format(item)
+      end
+    end
+
+    def match_result(match_item)
+      format(match_item)
+    end
+
+    def format(item)
+      {
+        "data": [
+          {
+            "id": item.id.to_s,
+            "category": item.category.name.to_s,
+            "body": item.body.to_s,
+            "created_at": item.created_at.to_i.to_s,
+          },
+        ],
+      }
+    end
 
     def category_params
       params.permit(:name)
