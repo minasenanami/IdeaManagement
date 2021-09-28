6461d72ed839f04863f3c7a02d185293abfd56ac
僚 <>
2021-09-28 16:15:54 +0900

    一覧取得機能の修正

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -7,38 +7,32 @@ class Api::V1::CategoriesController < ApplicationController
 
   def index
     category_word = Category.where(name: params[:name])
-    result = Idea.find_by(category_id: category_word.ids)
-    no_match = Idea.order(created_at: :asc).includes(:category)
+    result = Idea.where(category_id: category_word.ids).includes(:category)
 
-    if result
-      render json: match_result(result)
+    if result.any?
+      render json: { data: match_result(result) }
+    elsif params[:name].blank?
+      no_match = Idea.order(created_at: :asc).includes(:category)
+      render json: { data: match_result(no_match) }
     else
-      render json: not_match_result(no_match)
+      render json: { status: "404" }
     end
   end
 
   private
 
-    def not_match_result(not_match_items)
-      not_match_items.map do |item|
+    def match_result(match_items)
+      match_items.map do |item|
         format(item)
       end
     end
 
-    def match_result(match_item)
-      format(match_item)
-    end
-
     def format(item)
       {
-        "data": [
-          {
-            "id": item.id.to_s,
-            "category": item.category.name.to_s,
-            "body": item.body.to_s,
-            "created_at": item.created_at.to_i.to_s,
-          },
-        ],
+        "id": item.id.to_s,
+        "category": item.category.name.to_s,
+        "body": item.body.to_s,
+        "created_at": item.created_at.to_i.to_s,
       }
     end
 
