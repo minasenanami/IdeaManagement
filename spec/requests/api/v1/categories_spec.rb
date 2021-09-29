402ebb206b9cea62481d61439cd986aaf9df600e
僚 <>
2021-09-29 12:53:31 +0900

    検索したカテゴリーが存在する時に該当のアイデア一覧を返却する機能のテストを実装

--- spec/requests/api/v1/categories_spec.rb
+++ spec/requests/api/v1/categories_spec.rb
@@ -26,7 +26,7 @@ RSpec.describe "Api::V1::Categories", type: :request do
       end
     end
 
-    context "全情報がから無いとき" do
+    context "全情報が空欄のとき" do
       let(:params) { attributes_for(:category, name: nil).merge(attributes_for(:idea, body: nil)) }
       it "作成に失敗する" do
         expect { subject }.to raise_error(ActiveRecord::RecordInvalid)
@@ -44,6 +44,39 @@ RSpec.describe "Api::V1::Categories", type: :request do
       let(:params) { attributes_for(:category).merge(attributes_for(:idea, body: nil)) }
       it "作成に失敗する" do
         expect { subject }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+  end
+
+  describe "GET /api/v1/categories" do
+    subject { get(api_v1_categories_path, params: params) }
+
+    let!(:ideas) { create_list(:idea, idea_count) }
+    let(:idea_count) { 4 }
+    context "検索したカテゴリーのアイテムが存在する時" do
+      let!(:category_item) { create(:category) }
+      let!(:idea1) { create(:idea, category: category_item) }
+      let!(:idea2) { create(:idea, category: category_item) }
+      let!(:idea3) { create(:idea, category: category_item) }
+      let(:params) { attributes_for(:category, name: category_item.name) }
+      it "指定したカテゴリーを持つアイデア一覧を返す" do
+        subject
+        res = JSON.parse(response.body)
+
+        expect(res["data"].count).to eq 3
+        expect(res["data"][0].keys).to eq ["id", "category", "body", "created_at"]
+        expect(res["data"][0]["category"]).to eq params[:name]
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "リクエストが空のとき" do
+      it "登録されているアイデアの一覧を返す" do
+      end
+    end
+
+    context "リクエストしたカテゴリーが存在しないとき" do
+      it "404を返す" do
       end
     end
   end
